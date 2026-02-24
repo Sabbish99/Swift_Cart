0b1bdbdd0b58ae9f12ae7d5bb7e6b87bd72eeb41
Sabbish99 <programmersabbir99@gmail.com>
2026-02-24 20:24:08 +0600

    add product

--- js/index.js
+++ js/index.js
@@ -11,16 +11,46 @@ const trendingNow = () => {
 
     const showAllProduct = (products) => {
         const trending = document.getElementById('trending-now')
+        const justThreeProduct = products.slice(0, 3)
         trending.innerHTML = ''
-        products.forEach(product => {
+        justThreeProduct.forEach(product => {
+
             // console.log(product);
             const div = document.createElement('div')
             div.classList.add('card')
             div.innerHTML = `
-            <h1 class="border">hehehe added</h1>
+           <div class="grid grid-flow-row">
+                <div class="border ">
+                    <div class="flex justify-center items-center">
+                        <img  src="${product.image}">
+                    </div>
+                    <div class="p-4">
+         
+                        <div class=" flex justify-between items-center ">
+                            <p class="p-2 rounded-sm  text-blue-700 font-semibold bg-blue-300 text-xs">
+                                ${product.category}
+                            </p>
+                            <p><i class="fa-solid fa-star text-yellow-500"></i> ${product.rating.rate} (${product.rating.count})</p>
+                        </div>
+
+      
+                        <div class= "border p-2 mt-2">
+                            <h4 class = "font-semibold my-2">${product.title}</h4>
+                            <h3 class="font-bold text-lg">$${product.price}</h3>
+                        </div>
+                        <div class="flex justify-between my-4">
+                            <button class="detail-btn"><i class="fa-regular fa-eye"></i> Details</button>
+                            <button class="add-cart-btn"><i class="fa-solid fa-cart-shopping"></i> Add</button>
+                        </div>
+                    
+                    </div>
+            </div>
+           </div>
+            
             
             `
             trending.append(div)
+
         })
 
     }
